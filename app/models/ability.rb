c30b9275322dd16a40ca6f0471b80d7017852f7f
fatiha <fathiato@gmail.com>
2023-08-01 14:34:57 +0100

    sorting linters

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,6 +3,7 @@ class Ability
   def initialize(user)
     # can :read, Recipe, public: true
     return unless recipe.public == true
+
     can :read, Recipe, public: true
 
     # additional permissions for logged in users (they can read their own recipe)
@@ -14,10 +15,8 @@ class Ability
     # can(:read, Comment, user:)
     # can :destroy, Comment.where(author_id: user.id)
 
-
     # can :manage, :all
     # can :destroy, Post
     # can :destroy, Comment
   end
 end
-
